22c99733b08472eaa17d48d709483794966e8e55
DeshinMichael <deshinmisha26@gmail.com>
2023-07-05 16:50:18 +0800

    Create project

---
+++ app/src/main/java/com/example/nasaimagesbook/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.nasaimagesbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
